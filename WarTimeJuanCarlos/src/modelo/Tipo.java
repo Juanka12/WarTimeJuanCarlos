c1244ca950ed371bf9c3d84b80a35dbdd518b37d
Juanka12 <juancdmh@gmail.com>
2020-05-17 02:30:21 +0200

    update

--- WarTimeJuanCarlos/src/modelo/Tipo.java
+++ WarTimeJuanCarlos/src/modelo/Tipo.java
@@ -3,8 +3,8 @@ package modelo;
 public enum Tipo {
 	infanteria(1), caballeria(2), arqueria(1);
 	private int step;
-	Tipo(int i) {
-		// TODO Auto-generated constructor stub
+	Tipo(int step) {
+		this.step = step;
 	}
 	public int getStep() {
 		return step;
